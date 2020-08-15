babf32c0dd1f68cdc3755cb5097a4d87f084b4d2
M1racle.Hao <crazyhl@163.com>
2020-08-15 20:16:01 +0800

    提交获取proxy前的代码

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"parseAndCombineMyClashRules/concurrent_map"
+	"parseAndCombineMyClashRules/model"
 	"parseAndCombineMyClashRules/utils"
 	"path/filepath"
+	"sync"
 )
 
 var absPath string
@@ -109,7 +112,88 @@ func parseRule(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(baseRuleMap)
+	// 获取配置文件输入的订阅地址
+	pullProxySource := userConfigMap["pull-proxy-source"]
+	if pullProxySource == nil {
+		return
+	}
+	pullProxySourceMap := pullProxySource.([]interface{})
+	pullProxySourceMapLength := len(pullProxySourceMap)
+	if pullProxySourceMapLength <= 0 {
+		log.Printf("请求url: %s，请求IP: %s\n，没有获取到订阅地址", r.URL, utils.GetRequestIp(r))
+	}
+	// 多线程获取订阅
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(pullProxySourceMapLength)
+	proxyMap := concurrent_map.New()
+	proxyGroupMap := concurrent_map.New()
+	for _, proxySource := range pullProxySourceMap {
+		go func(proxySource interface{}) {
+			proxySourceMap := proxySource.(map[interface{}]interface{})
+			// 获取订阅
+			httpResponse, httpRequestErr := http.Get(proxySourceMap["url"].(string))
+			if httpRequestErr != nil {
+				log.Printf(
+					"请求url: %s，请求IP: %s, 订阅地址: %s, 报错内容：%v\n，没有获取到订阅地址",
+					r.URL,
+					utils.GetRequestIp(r),
+					proxySourceMap["url"].(string),
+					httpRequestErr.Error(),
+				)
+			}
+			fmt.Println(httpResponse)
+			// 插入到自定义组
+			proxyGroupMap.Set(proxySourceMap["name"].(string), nil)
+			fmt.Println(proxySourceMap["name"])
+			waitGroup.Done()
+		}(proxySource)
+	}
+	waitGroup.Wait()
+	fmt.Println(proxyGroupMap.Map)
+	fmt.Println(proxyMap.Map)
+	// 构造一些前置的参数
+	outputConfig := model.OutputConfig{}
+
+	outputConfig.Port = getConfigFieldValue("port", userConfigMap, baseRuleMap)
+	outputConfig.SocksPort = getConfigFieldValue("socks-port", userConfigMap, baseRuleMap)
+	outputConfig.RedirPort = getConfigFieldValue("redir-port", userConfigMap, baseRuleMap)
+	outputConfig.MixedPort = getConfigFieldValue("mixed-port", userConfigMap, baseRuleMap)
+
+	authentication := getConfigFieldValue("authentication", userConfigMap, baseRuleMap)
+	if authentication != nil {
+		outputConfig.Authentication = authentication.([]interface{})
+	} else {
+		outputConfig.Authentication = nil
+	}
+
+	outputConfig.AllowLan = getConfigFieldValue("allow-lan", userConfigMap, baseRuleMap)
+	outputConfig.BindAddress = getConfigFieldValue("bind-address", userConfigMap, baseRuleMap)
+	outputConfig.Mode = getConfigFieldValue("mode", userConfigMap, baseRuleMap)
+	outputConfig.LogLevel = getConfigFieldValue("log-level", userConfigMap, baseRuleMap)
+	outputConfig.Ipv6 = getConfigFieldValue("ipv6", userConfigMap, baseRuleMap)
+	outputConfig.ExternalController = getConfigFieldValue("external-controller", userConfigMap, baseRuleMap)
+	outputConfig.ExternalUi = getConfigFieldValue("external-ui", userConfigMap, baseRuleMap)
+	outputConfig.Secret = getConfigFieldValue("secret", userConfigMap, baseRuleMap)
+	outputConfig.InterfaceName = getConfigFieldValue("interface-name", userConfigMap, baseRuleMap)
+
+	hosts := getConfigFieldValue("hosts", userConfigMap, baseRuleMap)
+	if hosts != nil {
+		outputConfig.Hosts = hosts.(map[interface{}]interface{})
+	} else {
+		outputConfig.Hosts = nil
+	}
+
+	dns := getConfigFieldValue("dns", userConfigMap, baseRuleMap)
+	if dns != nil {
+		outputConfig.Dns = dns.(map[interface{}]interface{})
+	} else {
+		outputConfig.Dns = nil
+	}
+
+	outputConfig.Rules = getConfigFieldMergeValueArr("rules", userConfigMap, baseRuleMap)
+
+	outputConfig.RuleProviders = getConfigFieldMergeValueMap("rule-providers", userConfigMap, baseRuleMap)
+	outputConfig.ProxyProviders = getConfigFieldMergeValueMap("proxy-providers", userConfigMap, baseRuleMap)
 
 	//
 	//// 规则获取完成后多线程拉取用户订阅
@@ -159,6 +243,47 @@ func parseRule(w http.ResponseWriter, r *http.Request) {
 	//	readyToWriteRule = my_interface.MergeBaseRule(baseRule, customConfig, proxyArr.Map)
 	//}
 	//
-	//marshalRule, _ := yaml.Marshal(&readyToWriteRule)
-	//fmt.Fprintln(w, string(marshalRule))
+	marshalRule, _ := yaml.Marshal(&outputConfig)
+	fmt.Fprintln(w, string(marshalRule))
+}
+
+// 获取配置文件，二选一的，优先返回用户设置的，其次返回基础配置的，最后返回空
+func getConfigFieldValue(fieldName string, userConfigMap map[string]interface{}, baseRuleConfigMap map[string]interface{}) interface{} {
+	if userConfigMap[fieldName] != nil {
+		return userConfigMap[fieldName]
+	} else if baseRuleConfigMap[fieldName] != nil {
+		return baseRuleConfigMap[fieldName]
+	} else {
+		return nil
+	}
+	return nil
+}
+
+// 把用户自定义设置和基础配置合并后返回
+func getConfigFieldMergeValueArr(fieldName string, userConfigMap map[string]interface{}, baseRuleConfigMap map[string]interface{}) []interface{} {
+	var valueSlice []interface{}
+	if userConfigMap[fieldName] != nil {
+		valueSlice = append(valueSlice, userConfigMap[fieldName].([]interface{})...)
+	}
+	if baseRuleConfigMap[fieldName] != nil {
+		valueSlice = append(valueSlice, baseRuleConfigMap[fieldName].([]interface{})...)
+	}
+	return valueSlice
+}
+
+func getConfigFieldMergeValueMap(fieldName string, userConfigMap map[string]interface{}, baseRuleConfigMap map[string]interface{}) map[interface{}]interface{} {
+	valueMap := make(map[interface{}]interface{})
+	if baseRuleConfigMap[fieldName] != nil {
+		baseRuleConfigValueMap := baseRuleConfigMap[fieldName].(map[interface{}]interface{})
+		for key := range baseRuleConfigValueMap {
+			valueMap[key] = baseRuleConfigValueMap[key]
+		}
+	}
+	if userConfigMap[fieldName] != nil {
+		userConfigValueMap := userConfigMap[fieldName].(map[interface{}]interface{})
+		for key := range userConfigValueMap {
+			valueMap[key] = userConfigValueMap[key]
+		}
+	}
+	return valueMap
 }

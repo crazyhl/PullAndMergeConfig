16f0121f30c066582afe1b054308b0680d8c23b0
M1racle.Hao <crazyhl@163.com>
2022-04-26 14:16:03 +0800

    抽象部分方法

--- main.go
+++ main.go
@@ -56,66 +56,121 @@ func main() {
 }
 
 func parseRule(w http.ResponseWriter, r *http.Request) {
-	// 获取配置参数
-	configFileNameArr, getConfigFileNameOk := r.URL.Query()["name"]
-	if getConfigFileNameOk == false {
+	userConfigMap, baseRuleMap, done := parseRequestParams(r)
+	if done {
 		return
 	}
-	configFileName := configFileNameArr[0]
-	configFile, readConfigFileErr := ioutil.ReadFile(absPath + "/config/" + configFileName + ".yaml")
-	if readConfigFileErr != nil {
-		log.Printf("error: %v\n", readConfigFileErr)
-		log.Println("读入配置文件失败了，检查配置文件是否存在")
+
+	proxyArr, proxyGroupArr, proxyNameArr, done2 := getProxies(userConfigMap, r)
+	if done2 {
 		return
 	}
-	// 解析本地配置文件
-	userConfigMap := make(map[string]interface{})
-	err := yaml.Unmarshal([]byte(configFile), &userConfigMap)
-	if err != nil {
-		log.Fatalf("error: %v", err)
+
+	outPutClash(w, userConfigMap, baseRuleMap, proxyArr, proxyGroupArr, proxyNameArr)
+}
+
+func outPutClash(w http.ResponseWriter, userConfigMap map[string]interface{}, baseRuleMap map[string]interface{}, proxyArr []map[interface{}]interface{}, proxyGroupArr []map[interface{}]interface{}, proxyNameArr []interface{}) {
+	// 构造一些前置的参数
+	outputConfig := model.OutputConfig{}
+
+	outputConfig.Port = getConfigFieldValue("port", userConfigMap, baseRuleMap)
+	outputConfig.SocksPort = getConfigFieldValue("socks-port", userConfigMap, baseRuleMap)
+	outputConfig.RedirPort = getConfigFieldValue("redir-port", userConfigMap, baseRuleMap)
+	outputConfig.MixedPort = getConfigFieldValue("mixed-port", userConfigMap, baseRuleMap)
+
+	authentication := getConfigFieldValue("authentication", userConfigMap, baseRuleMap)
+	if authentication != nil {
+		outputConfig.Authentication = authentication.([]interface{})
+	} else {
+		outputConfig.Authentication = nil
 	}
 
-	// 获取请求参数的配置项
-	baseConfigName := r.URL.Query().Get("baseName")
-	baseConfigRequestUrl := ""
+	outputConfig.AllowLan = getConfigFieldValue("allow-lan", userConfigMap, baseRuleMap)
+	outputConfig.BindAddress = getConfigFieldValue("bind-address", userConfigMap, baseRuleMap)
+	outputConfig.Mode = getConfigFieldValue("mode", userConfigMap, baseRuleMap)
+	outputConfig.LogLevel = getConfigFieldValue("log-level", userConfigMap, baseRuleMap)
+	outputConfig.Ipv6 = getConfigFieldValue("ipv6", userConfigMap, baseRuleMap)
+	outputConfig.ExternalController = getConfigFieldValue("external-controller", userConfigMap, baseRuleMap)
+	outputConfig.ExternalUi = getConfigFieldValue("external-ui", userConfigMap, baseRuleMap)
+	outputConfig.Secret = getConfigFieldValue("secret", userConfigMap, baseRuleMap)
+	outputConfig.InterfaceName = getConfigFieldValue("interface-name", userConfigMap, baseRuleMap)
 
-	if baseConfigName != "" {
-		// 如果获取成功就去找配置文件
-		for _, baseConfig := range userConfigMap["base-config"].([]interface{}) {
-			baseConfigMap := baseConfig.(map[interface{}]interface{})
-			if baseConfigMap["name"] == baseConfigName {
-				baseConfigRequestUrl = baseConfigMap["url"].(string)
+	hosts := getConfigFieldValue("hosts", userConfigMap, baseRuleMap)
+	if hosts != nil {
+		outputConfig.Hosts = hosts.(map[interface{}]interface{})
+	} else {
+		outputConfig.Hosts = nil
+	}
+
+	dns := getConfigFieldValue("dns", userConfigMap, baseRuleMap)
+	if dns != nil {
+		outputConfig.Dns = dns.(map[interface{}]interface{})
+	} else {
+		outputConfig.Dns = nil
+	}
+
+	outputConfig.Rules = getConfigFieldMergeValueArr("rules", userConfigMap, baseRuleMap)
+
+	outputConfig.RuleProviders = getConfigFieldMergeValueMap("rule-providers", userConfigMap, baseRuleMap)
+	// proxy providers
+	filterProxyProvier := userConfigMap["filter-proxy-providers"]
+	var filterProxyProviderArr []interface{}
+	if filterProxyProvier != nil {
+		filterProxyProviderArr = filterProxyProvier.([]interface{})
+	}
+	outputConfig.ProxyProviders = getConfigFieldMergeValueMap("proxy-providers", userConfigMap, baseRuleMap)
+	for providerName := range outputConfig.ProxyProviders {
+		for _, proxyProvider := range filterProxyProviderArr {
+			if providerName == proxyProvider {
+				delete(outputConfig.ProxyProviders, providerName)
 			}
 		}
-	} else {
-		log.Printf("请求url: %s，请求IP: %s\n，没有输入配置文件名称", r.URL, utils.GetRequestIp(r))
-		return
+	}
+	// 构造 proxy
+	outputConfig.Proxies = proxyArr
+	// 构造 proxyGroup
+	filterProxyGroup := userConfigMap["filter-proxy-groups"]
+	var filterProxyGroupArr []interface{}
+	if filterProxyGroup != nil {
+		filterProxyGroupArr = filterProxyGroup.([]interface{})
+	}
+	var outputProxyGroupMap []map[interface{}]interface{}
+	// 加入上面几组订阅的单独group
+	outputProxyGroupMap = append(outputProxyGroupMap, proxyGroupArr...)
+	userConfigProxyGroups := userConfigMap["proxy-groups"]
+	if userConfigProxyGroups != nil {
+		userConfigProxyGroupMapArr := generateProxyNameToGroup(userConfigProxyGroups, proxyNameArr, filterProxyGroupArr)
+		outputProxyGroupMap = append(outputProxyGroupMap, userConfigProxyGroupMapArr...)
+	}
+	baseRuleProxyGroups := baseRuleMap["proxy-groups"]
+	if baseRuleProxyGroups != nil {
+		baseRuleProxyGroupMapArr := generateProxyNameToGroup(baseRuleProxyGroups.([]interface{}), proxyNameArr, filterProxyGroupArr)
+		// 重新整理一下需要把一些 group name 加入到 select 组中
+		var noneSelectGroupName []interface{}
+		for _, proxyGroupMap := range baseRuleProxyGroupMapArr {
+			if proxyGroupMap["type"] != "select" {
+				noneSelectGroupName = append(noneSelectGroupName, proxyGroupMap["name"])
+			}
+		}
+		for idx, proxyGroupMap := range baseRuleProxyGroupMapArr {
+			if proxyGroupMap["type"] == "select" {
+				baseRuleProxyGroupMapArr[idx]["proxies"] = append(noneSelectGroupName, baseRuleProxyGroupMapArr[idx]["proxies"].([]interface{})...)
+			}
+		}
+		outputProxyGroupMap = append(outputProxyGroupMap, baseRuleProxyGroupMapArr...)
 	}
 
-	if baseConfigRequestUrl == "" {
-		log.Printf("请求url: %s，请求IP: %s\n，没有找到对应配置文件", r.URL, utils.GetRequestIp(r))
-	}
+	outputConfig.ProxyGroups = outputProxyGroupMap
 
-	// 拉取基础配置
-	baseRuleBody, baseRuleErr := utils.HttpGet(baseConfigRequestUrl)
-	if baseRuleErr != nil {
-		log.Println(baseRuleErr)
-		log.Printf("请求url: %s，请求IP: %s\n，拉取基础规则失败，请检查网络！！！！", r.URL, utils.GetRequestIp(r))
-		return
-	}
+	marshalRule, _ := yaml.Marshal(&outputConfig)
+	fmt.Fprintln(w, string(marshalRule))
+}
 
-	baseRuleMap := make(map[string]interface{})
-	unmarshalReadyToWriteRuleErr := yaml.Unmarshal(baseRuleBody, &baseRuleMap)
-	if unmarshalReadyToWriteRuleErr != nil {
-		log.Printf("error: %v\n", unmarshalReadyToWriteRuleErr)
-		log.Println("解析基础规则失败，请检查 url 是否正确 ！！！")
-		return
-	}
-
+func getProxies(userConfigMap map[string]interface{}, r *http.Request) ([]map[interface{}]interface{}, []map[interface{}]interface{}, []interface{}, bool) {
 	// 获取配置文件输入的订阅地址
 	pullProxySource := userConfigMap["pull-proxy-source"]
 	if pullProxySource == nil {
-		return
+		return nil, nil, nil, true
 	}
 	pullProxySourceMap := pullProxySource.([]interface{})
 	pullProxySourceMapLength := len(pullProxySourceMap)
@@ -212,100 +267,66 @@ func parseRule(w http.ResponseWriter, r *http.Request) {
 		}(proxySource)
 	}
 	waitGroup.Wait()
-	// 构造一些前置的参数
-	outputConfig := model.OutputConfig{}
+	return proxyArr, proxyGroupArr, proxyNameArr, false
+}
 
-	outputConfig.Port = getConfigFieldValue("port", userConfigMap, baseRuleMap)
-	outputConfig.SocksPort = getConfigFieldValue("socks-port", userConfigMap, baseRuleMap)
-	outputConfig.RedirPort = getConfigFieldValue("redir-port", userConfigMap, baseRuleMap)
-	outputConfig.MixedPort = getConfigFieldValue("mixed-port", userConfigMap, baseRuleMap)
-
-	authentication := getConfigFieldValue("authentication", userConfigMap, baseRuleMap)
-	if authentication != nil {
-		outputConfig.Authentication = authentication.([]interface{})
-	} else {
-		outputConfig.Authentication = nil
+func parseRequestParams(r *http.Request) (map[string]interface{}, map[string]interface{}, bool) {
+	// 获取配置参数
+	configFileNameArr, getConfigFileNameOk := r.URL.Query()["name"]
+	if getConfigFileNameOk == false {
+		return nil, nil, true
+	}
+	configFileName := configFileNameArr[0]
+	configFile, readConfigFileErr := ioutil.ReadFile(absPath + "/config/" + configFileName + ".yaml")
+	if readConfigFileErr != nil {
+		log.Printf("error: %v\n", readConfigFileErr)
+		log.Println("读入配置文件失败了，检查配置文件是否存在")
+		return nil, nil, true
+	}
+	// 解析本地配置文件
+	userConfigMap := make(map[string]interface{})
+	err := yaml.Unmarshal([]byte(configFile), &userConfigMap)
+	if err != nil {
+		log.Fatalf("error: %v", err)
 	}
 
-	outputConfig.AllowLan = getConfigFieldValue("allow-lan", userConfigMap, baseRuleMap)
-	outputConfig.BindAddress = getConfigFieldValue("bind-address", userConfigMap, baseRuleMap)
-	outputConfig.Mode = getConfigFieldValue("mode", userConfigMap, baseRuleMap)
-	outputConfig.LogLevel = getConfigFieldValue("log-level", userConfigMap, baseRuleMap)
-	outputConfig.Ipv6 = getConfigFieldValue("ipv6", userConfigMap, baseRuleMap)
-	outputConfig.ExternalController = getConfigFieldValue("external-controller", userConfigMap, baseRuleMap)
-	outputConfig.ExternalUi = getConfigFieldValue("external-ui", userConfigMap, baseRuleMap)
-	outputConfig.Secret = getConfigFieldValue("secret", userConfigMap, baseRuleMap)
-	outputConfig.InterfaceName = getConfigFieldValue("interface-name", userConfigMap, baseRuleMap)
+	// 获取请求参数的配置项
+	baseConfigName := r.URL.Query().Get("baseName")
+	baseConfigRequestUrl := ""
 
-	hosts := getConfigFieldValue("hosts", userConfigMap, baseRuleMap)
-	if hosts != nil {
-		outputConfig.Hosts = hosts.(map[interface{}]interface{})
-	} else {
-		outputConfig.Hosts = nil
-	}
-
-	dns := getConfigFieldValue("dns", userConfigMap, baseRuleMap)
-	if dns != nil {
-		outputConfig.Dns = dns.(map[interface{}]interface{})
-	} else {
-		outputConfig.Dns = nil
-	}
-
-	outputConfig.Rules = getConfigFieldMergeValueArr("rules", userConfigMap, baseRuleMap)
-
-	outputConfig.RuleProviders = getConfigFieldMergeValueMap("rule-providers", userConfigMap, baseRuleMap)
-	// proxy providers
-	filterProxyProvier := userConfigMap["filter-proxy-providers"]
-	var filterProxyProviderArr []interface{}
-	if filterProxyProvier != nil {
-		filterProxyProviderArr = filterProxyProvier.([]interface{})
-	}
-	outputConfig.ProxyProviders = getConfigFieldMergeValueMap("proxy-providers", userConfigMap, baseRuleMap)
-	for providerName := range outputConfig.ProxyProviders {
-		for _, proxyProvider := range filterProxyProviderArr {
-			if providerName == proxyProvider {
-				delete(outputConfig.ProxyProviders, providerName)
+	if baseConfigName != "" {
+		// 如果获取成功就去找配置文件
+		for _, baseConfig := range userConfigMap["base-config"].([]interface{}) {
+			baseConfigMap := baseConfig.(map[interface{}]interface{})
+			if baseConfigMap["name"] == baseConfigName {
+				baseConfigRequestUrl = baseConfigMap["url"].(string)
 			}
 		}
-	}
-	// 构造 proxy
-	outputConfig.Proxies = proxyArr
-	// 构造 proxyGroup
-	filterProxyGroup := userConfigMap["filter-proxy-groups"]
-	var filterProxyGroupArr []interface{}
-	if filterProxyGroup != nil {
-		filterProxyGroupArr = filterProxyGroup.([]interface{})
-	}
-	var outputProxyGroupMap []map[interface{}]interface{}
-	// 加入上面几组订阅的单独group
-	outputProxyGroupMap = append(outputProxyGroupMap, proxyGroupArr...)
-	userConfigProxyGroups := userConfigMap["proxy-groups"]
-	if userConfigProxyGroups != nil {
-		userConfigProxyGroupMapArr := generateProxyNameToGroup(userConfigProxyGroups, proxyNameArr, filterProxyGroupArr)
-		outputProxyGroupMap = append(outputProxyGroupMap, userConfigProxyGroupMapArr...)
-	}
-	baseRuleProxyGroups := baseRuleMap["proxy-groups"]
-	if baseRuleProxyGroups != nil {
-		baseRuleProxyGroupMapArr := generateProxyNameToGroup(baseRuleProxyGroups.([]interface{}), proxyNameArr, filterProxyGroupArr)
-		// 重新整理一下需要把一些 group name 加入到 select 组中
-		var noneSelectGroupName []interface{}
-		for _, proxyGroupMap := range baseRuleProxyGroupMapArr {
-			if proxyGroupMap["type"] != "select" {
-				noneSelectGroupName = append(noneSelectGroupName, proxyGroupMap["name"])
-			}
-		}
-		for idx, proxyGroupMap := range baseRuleProxyGroupMapArr {
-			if proxyGroupMap["type"] == "select" {
-				baseRuleProxyGroupMapArr[idx]["proxies"] = append(noneSelectGroupName, baseRuleProxyGroupMapArr[idx]["proxies"].([]interface{})...)
-			}
-		}
-		outputProxyGroupMap = append(outputProxyGroupMap, baseRuleProxyGroupMapArr...)
+	} else {
+		log.Printf("请求url: %s，请求IP: %s\n，没有输入配置文件名称", r.URL, utils.GetRequestIp(r))
+		return nil, nil, true
 	}
 
-	outputConfig.ProxyGroups = outputProxyGroupMap
+	if baseConfigRequestUrl == "" {
+		log.Printf("请求url: %s，请求IP: %s\n，没有找到对应配置文件", r.URL, utils.GetRequestIp(r))
+	}
 
-	marshalRule, _ := yaml.Marshal(&outputConfig)
-	fmt.Fprintln(w, string(marshalRule))
+	// 拉取基础配置
+	baseRuleBody, baseRuleErr := utils.HttpGet(baseConfigRequestUrl)
+	if baseRuleErr != nil {
+		log.Println(baseRuleErr)
+		log.Printf("请求url: %s，请求IP: %s\n，拉取基础规则失败，请检查网络！！！！", r.URL, utils.GetRequestIp(r))
+		return nil, nil, true
+	}
+
+	baseRuleMap := make(map[string]interface{})
+	unmarshalReadyToWriteRuleErr := yaml.Unmarshal(baseRuleBody, &baseRuleMap)
+	if unmarshalReadyToWriteRuleErr != nil {
+		log.Printf("error: %v\n", unmarshalReadyToWriteRuleErr)
+		log.Println("解析基础规则失败，请检查 url 是否正确 ！！！")
+		return nil, nil, true
+	}
+	return userConfigMap, baseRuleMap, false
 }
 
 // 获取配置文件，二选一的，优先返回用户设置的，其次返回基础配置的，最后返回空
